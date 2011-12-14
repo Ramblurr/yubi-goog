25a8f70a287df5af37811ead9d168ea1ccfc8339
Casey Link <unnamedrambler@gmail.com>
2011-12-13 19:00:21 -0500

    make --yubi the default when no opts are passed

--- yubi_goog.py
+++ yubi_goog.py
@@ -105,7 +105,7 @@ def error():
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        error()
+        yubi()
         sys.exit(1)
     if sys.argv[1] == "--generate":
         generate()
